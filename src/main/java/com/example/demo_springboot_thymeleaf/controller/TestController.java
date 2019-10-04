22f0088e11e4f38baba173175156504cd489c09b
CZWGOGO <925276703@qq.com>
2019-10-04 14:35:00 +0800

    遍历演示

--- src/main/java/com/example/demo_springboot_thymeleaf/controller/TestController.java
+++ src/main/java/com/example/demo_springboot_thymeleaf/controller/TestController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by CZWGOGO on 2019/10/4 13:28
  */
@@ -16,7 +19,16 @@ public class TestController {
         String htmlContent="<p style='color:red'>红色文字</p>";
         Product currentProduct=new Product(5,"product e",200);
         boolean testBoolean = false;
+        List<Product> ps = new ArrayList<>();
+        ps.add(new Product(1,"product a", 50));
+        ps.add(new Product(2,"product b", 100));
+        ps.add(new Product(3,"product c", 150));
+        ps.add(new Product(4,"product d", 200));
+        ps.add(currentProduct);
+        ps.add(new Product(6,"product f", 200));
+        ps.add(new Product(7,"product g", 200));
 
+        m.addAttribute("ps", ps);
         m.addAttribute("htmlContent",htmlContent);
         m.addAttribute("currentProduct",currentProduct);
         m.addAttribute("testBoolean", false);
